27a2192d5305c561924f8d7d8c582e7ae5e49028
Philyciana <179555132+Philyciana@users.noreply.github.com>
2024-10-23 17:36:10 -0400

    rough codefor user input to search through api

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,33 @@
-fetch("https://poetrydb.org/author,title/Shakespeare;Sonnet")
+fetch("https://poetrydb.org/author")
     .then(res => res.json())
     .then(data => {
-        data.forEach(poem => {
-            const markup = `<div>
-            <h3>${poem.title}</h3>
-            <p>${poem.lines}</p>
-            <p>${poem.linecount}</p></div>`;
+        data.authors.forEach(author => {
+            const markup = `<input type="checkbox" id="${author}">${author}</input><br>`;
             document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
         });
     })
     .catch(error => console.log('Error not loaded', error))
+
+
+document.getElementById('search').addEventListener('submit', function(sub){
+    sub.preventDefault();
+    let selectedcontainter = [];
+    const authorSelected = document.querySelectorAll('input[name="author"]:checked');
+    authorSelected.forEach(selected => {
+        selectedcontainter.push(selected.value);
+    })
+    let authorList = selectedcontainer.join(';');
+    let inputAuthorapi =  'https://poetrydb.org/author/${selected}';
+    fetch(inputAuthorapi)
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(poem => {
+                const markup = `<div><h3>${poem.title}</h3>
+                <p> Written by: ${poem.author}</p>
+                <ul>${poem.lines.map(line => `<li>${line}</li>`).join('')}</ul></div>`;
+                document.getElementById('results').insertAdjacentHTML('beforeend', markup);
+            })
+
+        })
+})
+
